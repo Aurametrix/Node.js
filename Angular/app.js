4e992694f1f0b1beca07994235c6e66df8c24bd5
Aurametrix <irene@aurametrix.com>
2014-09-22 16:54:39 -0700

    adding controller
    wrapping entire application into a clojure — function for the var and adding controller to get data on the page

--- Angular/app.js
+++ Angular/app.js
@@ -1,2 +1,9 @@
-var app;
-var app = angular.module('gemStore', [ ]);
+(function(){
+  var gem = { name: 'Azurite', price: 2.95 };
+  var app = angular.module('gemStore', []);
+  
+   app.controller('StoreController', function(){
+                 this.product=gem;
+                 });  
+                 
+})();
