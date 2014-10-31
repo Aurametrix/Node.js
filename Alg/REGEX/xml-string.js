a306629fb69a5e350858563927213411ad2ce173
Aurametrix <irene@aurametrix.com>
2014-10-30 20:38:15 -0700

    parse an XML string into an XML DOM object
    Internet Explorer uses the loadXML() method to parse an XML string, while other browsers use the DOMParser object.

---
+++ Alg/REGEX/xml-string.js
@@ -0,0 +1,17 @@
+txt="<bookstore><book>";
+txt=txt+"<title>Everyday Italian</title>";
+txt=txt+"<author>Giada De Laurentiis</author>";
+txt=txt+"<year>2005</year>";
+txt=txt+"</book></bookstore>";
+
+if (window.DOMParser)
+  {
+  parser=new DOMParser();
+  xmlDoc=parser.parseFromString(txt,"text/xml");
+  }
+else // Internet Explorer
+  {
+  xmlDoc=new ActiveXObject("Microsoft.XMLDOM");
+  xmlDoc.async=false;
+  xmlDoc.loadXML(txt); 
+  }
