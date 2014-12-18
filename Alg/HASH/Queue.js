c29be620c68089035cdc73f73761049336bda683
Aurametrix <irene@aurametrix.com>
2014-12-17 21:20:36 -0800

    queue

---
+++ Alg/HASH/Queue.js
@@ -0,0 +1,5 @@
+var queue = [];
+queue.push(2);         // queue is now [2]
+queue.push(5);         // queue is now [2, 5]
+var i = queue.shift(); // queue is now [5]
+alert(i);              // displays 2
