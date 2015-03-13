8aefec6b90240065f0e2703b1971dfb7d2a0ec0a
Aurametrix <irene@aurametrix.com>
2015-03-13 11:42:22 -0700

    post request example

---
+++ Angular/post.js
@@ -0,0 +1,10 @@
+// Simple POST request example (passing data) :
+$http.post('/someUrl', {msg:'hello word!'}).
+  success(function(data, status, headers, config) {
+    // this callback will be called asynchronously
+    // when the response is available
+  }).
+  error(function(data, status, headers, config) {
+    // called asynchronously if an error occurs
+    // or server returns response with an error status.
+  });
