92ca93011df570c24bca9bb3d23f7478cb977cee
Aurametrix <irene@aurametrix.com>
2016-01-16 18:01:59 -0800

    Java​Script​Next - ES6 Syntax

--- Alg/REGEX/palindrome.js
+++ Alg/REGEX/palindrome.js
@@ -1,5 +1,9 @@
 function isPalindrome(str) {
   return str === str.split("").reverse().join("");
+
+  // ES6
+  // var isPal = str => str === str.split("").reverse().join("");
+  
 }
  
 console.log(isPalindrome("ingirumimusnocteetconsumimurigni"));
