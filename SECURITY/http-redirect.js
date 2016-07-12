4afd82339a89b8f586c6bb30805a446c5b322523
Aurametrix <irene@aurametrix.com>
2016-07-11 20:14:41 -0700

    simple HTTP request handler for redirection

---
+++ SECURITY/http-redirect.js
@@ -0,0 +1,6 @@
+	http.createServer(​function​ (req, res) {
+​ 	    res.writeHead(301, {
+​ 	        Location: ​'https://'​ + req.headers.host + req.url
+​ 	    });
+​ 	    res.end();
+​ 	}).listen(80);
