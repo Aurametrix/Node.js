a53fd47174575b5b2fbed45ef37344f277f2efb9
Aurametrix <irene@aurametrix.com>
2017-02-15 20:30:01 -0800

    available functions

---
+++ Blockchain/blockstack.js
@@ -0,0 +1,36 @@
+{ profiles: 
+   { signTokenRecord: [Function: signTokenRecord],
+     signTokenRecords: [Function: signTokenRecords],
+     getProfileFromTokens: [Function: getProfileFromTokens],
+     verifyTokenRecord: [Function: verifyTokenRecord],
+     prepareZoneFileForHostedFile: [Function: prepareZoneFileForHostedFile],
+     Profile: [Function: Profile],
+     Person: [Function: Person],
+     Organization: [Function: Organization],
+     CreativeWork: [Function: CreativeWork],
+     nextYear: [Function: nextYear] },
+  keychain: 
+   { getChildKeypair: [Function: getChildKeypair],
+     getEntropy: [Function: getEntropy],
+     PrivateKeychain: [Function: PrivateKeychain],
+     PublicKeychain: [Function: PublicKeychain],
+     numberToEntropy: [Function: numberToEntropy] },
+  tokens: 
+   { TokenSigner: [Function: TokenSigner],
+     TokenVerifier: [Function: TokenVerifier],
+     decodeToken: [Function: decodeToken],
+     MissingParametersError: [Function: MissingParametersError],
+     SECP256K1Client: 
+      { [Function: SECP256K1Client]
+        algorithmName: 'ES256K',
+        createHash: [Function],
+        loadPrivateKey: [Function],
+        loadPublicKey: [Function],
+        privateKeyToPublicKey: [Function],
+        signHash: [Function],
+        loadSignature: [Function],
+        verifyHash: [Function] } },
+  zones: 
+   { makeZoneFile: [Function: makeZoneFile],
+     parseZoneFile: [Function: parseZoneFile],
+     ZoneFile: [Function: ZoneFile] } }
