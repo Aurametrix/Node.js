8a39eec070b14c68196c76301766b966df6a413d
Aurametrix <irene@aurametrix.com>
2017-05-16 14:23:52 -0700

    AI Playbook - sentiment analysis
    https://news.ycombinator.com/item?id=14344034
    http://aiplaybook.a16z.com/docs/guides/nlp

---
+++ NLP/sentiment.js
@@ -0,0 +1,20 @@
+{
+    "score": 3,
+    "comparative": 0.5,
+    "tokens": [
+        "the",
+        "weather",
+        "today",
+        "is",
+        "really",
+        "nice"
+    ],
+    "words": [
+        "nice"
+    ],
+    "positive": [
+        "nice"
+    ],
+    "negative": [],
+    "scoreRange": "returns values between -10 and 10"
+}
