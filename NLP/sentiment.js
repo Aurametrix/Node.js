edcd8f9c3f88d51975187afe1f198cbc9a77e2f7
Aurametrix <irene@aurametrix.com>
2017-05-16 14:26:45 -0700

    multilang - usage
    https://www.npmjs.com/package/sentiment-multilang

--- NLP/sentiment.js
+++ NLP/sentiment.js
@@ -1,20 +1,16 @@
-{
-    "score": 3,
-    "comparative": 0.5,
-    "tokens": [
-        "the",
-        "weather",
-        "today",
-        "is",
-        "really",
-        "nice"
-    ],
-    "words": [
-        "nice"
-    ],
-    "positive": [
-        "nice"
-    ],
-    "negative": [],
-    "scoreRange": "returns values between -10 and 10"
+// Require the sentiment-multilang module 
+var sentiment = require('sentiment-multilang');
+ 
+// Use the module to get sentiment from texts. 
+var r1 = sentiment('Cats are stupid.','en');
+console.dir(r1);        // Vote: 'negative' 
+ 
+var r2 = sentiment('Cats are totally amazing!','en');
+console.dir(r2);        // Vote: 'positive' 
+ 
+var r3 = sentiment('I gatti sono stupidi.','it');
+console.dir(r3);        // Vote: 'negative' 
+ 
+var r4 = sentiment('I gatti sono totalmente stupendi!','it');
+console.dir(r4);        // Vote: 'positive' -10 and 10"
 }
