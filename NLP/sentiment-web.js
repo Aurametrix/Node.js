da2fc531e7fcb5a180742b60211f18610e35eff9
Aurametrix <irene@aurametrix.com>
2017-05-16 14:29:39 -0700

    sentiment analysis over the web
    http://aiplaybook.a16z.com/docs/guides/ai-using

---
+++ NLP/sentiment-web.js
@@ -0,0 +1,19 @@
+GET /api/sentiment?phrase=Airplanes+with+the+shape+of+hawks+are+cool
+
+{
+     "score": 0.6000000238418579,
+     "magnitude": 0.6000000238418579,
+     "sentences": [
+         {
+             "text": {
+                 "content": "Airplanes with the shape of hawks are cool.",
+                 "beginOffset": -1
+             },
+             "sentiment": {
+                 "magnitude": 0.6000000238418579,
+                 "score": 0.6000000238418579
+             }
+         }
+     ],
+     "language": "en"
+ }
