d929f0fc09f709fdb0265a66da0fe9d00dce5da3
Aurametrix <irene@aurametrix.com>
2018-02-13 10:38:17 -0800

    apify analyzer
    https://news.ycombinator.com/item?id=16366275
    https://github.com/apifytech/act-page-analyzer
    https://www.apify.com/page-analyzer

---
+++ NLP/web-scapper.js
@@ -0,0 +1,6 @@
+{
+    // url to website, that is supposed to be analyzed
+    "url": "http://example.com",
+    // array of strings too look for on the website, if empty, search is skipped during analysis
+    "searchFor": ["About us"]
+}
