358da9e7946dc9418c9f776cafa74fd5b6c5218e
Aurametrix <irene@aurametrix.com>
2018-03-21 20:05:15 -0700

    canvas-based library
    https://news.ycombinator.com/item?id=16571827
    http://roughjs.com/

---
+++ VIZ/Rough.js
@@ -0,0 +1,15 @@
+rc.circle(50, 50, 80, { fill: 'red' }); // fill with red hachure
+rc.rectangle(120, 15, 80, 80, { fill: 'red' });
+rc.circle(50, 150, 80, {
+  fill: "rgb(10,150,10)",
+  fillWeight: 3 // thicker lines for hachure
+});
+rc.rectangle(220, 15, 80, 80, {
+  fill: 'red',
+  hachureAngle: 60, // angle of hachure,
+  hachureGap: 8
+});
+rc.rectangle(120, 105, 80, 80, {
+  fill: 'rgba(255,0,200,0.2)',
+  fillStyle: 'solid' // solid fill
+});
