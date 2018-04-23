ddffb0dea25f431b41ff279522ad11c7cebb7f95
Aurametrix <irene@aurametrix.com>
2018-04-23 15:35:12 -0700

    optimizing app's bundle
    http://web.jakoblind.no/webpack-config/
    https://news.ycombinator.com/item?id=16902220

---
+++ misc/webpack.config.js
@@ -0,0 +1,13 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  mode: 'development'
+}
+
+module.exports = config;
