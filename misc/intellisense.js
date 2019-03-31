7ba0d70dfc61ff6b2310acde2345558a08dfc6b4
Aurametrix <irene@aurametrix.com>
2019-03-30 21:06:36 -0400

    coc.nvim
    https://github.com/neoclide/coc.nvim
    https://news.ycombinator.com/item?id=19529557

---
+++ misc/intellisense.js
@@ -0,0 +1,10 @@
+"languageserver":{
+   "ccls": {
+    "command": "ccls",
+    "filetypes": ["c", "cpp", "objc", "objcpp"],
+    "trace.server": "verbose",
+    "initializationOptions": {
+      "cacheDirectory": "/tmp/ccls"
+    }
+  }
+}
