d1fdbae4f2a29929e60320adb9bdc848402a6178
Aurametrix <irene@aurametrix.com>
2019-10-23 21:08:58 -0400

    info from web services
    https://refl.me/docs
    Alternative: Telegram bot
    https://news.ycombinator.com/item?id=21337330

---
+++ IoT/reflme.js
@@ -0,0 +1,15 @@
+{
+    "refl.me":true,
+    "message":"Hello!"
+}
+
+
+{
+        "refl.me":true,
+        "title":"Test",
+        "message":"Hello!",
+        "image":"https://refl.me/img/reflme.png",
+        "go_link":"https://refl.me/",
+        "refresh":96,
+        "close":false
+}
