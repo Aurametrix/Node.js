7b7c19ae3da85a7bc2bc9fb45ab331b98e0de1a9
Aurametrix <irene@aurametrix.com>
2019-12-24 19:53:31 -0500

    Christmas viz
    https://www.dwitter.net/h/christmas

---
+++ VIZ/Christmas.js
@@ -0,0 +1,15 @@
+function u(t) {
+for(i=c.width|=0;i--;x.fillRect(S(a=i+t)*r+960,i/2,C(a)>0&&C(a)*r/2,19))s=i<1500,r=s?i/3:99,x.fillStyle=`hsl(${s*88} 33%${i%S(i)*77}%`
+}// 134/140
+
+function u(t) {
+c.width^=0
+x.fillRect(0,0,2e3,2e3)
+k=t<3?80*t:240
+x.fillStyle="#5e9"
+for(i=1;i<=k;i=i+.2)x.fillRect(3e3*(i%1)+i*S(i+t)-k,k+i*3*C(i/600),5,9)
+}// 140/140
+
+function u(t) {
+for(k=c.width|=n=960;k--;x.fillRect(n+(y/3+40)*C(a),y,7,99))j=k%n,a=j/62+((j/192|0)+(k>n))*3.2,x.fillStyle=R(b=S(a)*255,-b/2),y=(a+t*5)*30%n
+}// 140/140
