e9eb8af56b866864557b8e1d0f6a72f0082ef436
Aurametrix <irene@aurametrix.com>
2020-01-11 22:03:17 -0500

    interactive animations
    https://www.aminerman.com/kinesis/#/
    https://news.ycombinator.com/item?id=21991554

---
+++ VUE/kinesis.js
@@ -0,0 +1,11 @@
+import { KinesisContainer, KinesisElement} from ‘vue-kinesis’
+
+          <kinesis-container>
+              Here, you can put
+              <kinesis-element :strength="10">
+                whatever
+              </kinesis-element>
+              <kinesis-element :strength="20">
+                content!
+              </kinesis-element>
+          </kinesis-container>
