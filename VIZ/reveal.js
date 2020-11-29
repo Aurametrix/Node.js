b191267d4435cc40217b7a9b9a3745174ea38e4a
Aurametrix <irene@aurametrix.com>
2020-11-28 20:12:21 -0500

    open source HTML presentation framework
    https://news.ycombinator.com/item?id=25233136
    https://github.com/hakimel/reveal.js/wiki/Example-Presentations
    https://revealjs.com/code/

---
+++ VIZ/reveal.js
@@ -0,0 +1,12 @@
+// npm install revealjs-animated
+
+
+<link rel="stylesheet" href="lib/css/monokai.css">
+<script src="plugin/highlight/highlight.js"></script>
+<script>
+  Reveal.initialize({
+    plugins: [ RevealHighlight ]
+  });
+</script>
+
+Reveal.configure({ backgroundTransition: 'zoom' })
