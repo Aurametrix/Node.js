4d70027c411931d007362de081662ff133df71cb
Aurametrix <irene@aurametrix.com>
2023-12-26 08:54:31 -0500

    sketchy, hand-drawn-like, style
    https://news.ycombinator.com/item?id=38732049

--- VIZ/Rough.js
+++ VIZ/Rough.js
@@ -1,3 +1,7 @@
+// Full Rough.js API: https://github.com/rough-stuff/rough/wiki
+//  https://roughjs.com/
+// rough notation: https://roughnotation.com/
+
 rc.circle(50, 50, 80, { fill: 'red' }); // fill with red hachure
 rc.rectangle(120, 15, 80, 80, { fill: 'red' });
 rc.circle(50, 150, 80, {
